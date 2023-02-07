06555a329e5e48892aba3526b631db44c70945fa
Magicboyd <captaintylerwatson@hotmail.com>
2023-02-06 19:39:16 -0800

    File read and basic main while loop

--- main.cpp
+++ main.cpp
@@ -1,7 +1,72 @@
+/* Encryption Lab
+ * CSC4220
+ * Authored by Tyler Watson and Ammanuel Beyene
+*/
+
+//Imports
 #include <iostream>
+#include <fstream>
+#include <sstream>
 using std::cout;
 using std::endl;
+using std::string;
+using std::cin;
+using std::ifstream;
+using std::stringstream;
+
+//Method Headers
+void ReadFile(string fileName, string& fileContents);
+void WriteToFile(string fileContents);
+void EncryptString(string originalString, string& encryptedString);
+void decryptString(string encryptedString, string& decryptedString);
 
 int main(int argc, char* argv[]) {
-    cout << "Hello World" << endl;
+    bool continueProgram = true;
+    while (continueProgram) {
+        cout << "===================================" << endl;
+        cout << "What option would you like?" << endl;
+        cout << "Type '1' to encrypt a file." << endl;
+        cout << "Type '2' to decrypt a file." << endl;
+        cout << "Type '3' to exit the program." << endl;
+
+        string userInput;
+        cin >> userInput;
+
+        if (userInput == "1") {
+            cout << "===================================" << endl;
+            cout << "What is the file name?" << endl;
+            string fileName;
+            cin >> fileName;
+
+            string fileContents;
+            ReadFile(fileName, fileContents);
+            cout << fileContents;
+        } else if (userInput == "2") {
+            //Code for decrypting a file
+        } else if (userInput == "3") {
+            continueProgram = false;
+        } else {
+            cout << "Invalid Option, Try Again!" << endl;
+        }
+    }
+}
+
+//Purpose: takes in the name of a file and reads its contents (if the file exists)
+//Returns: returns a string which is representative of the file contents
+void ReadFile(string fileName, string& fileContents) {
+    ifstream file;
+    file.open(fileName);
+    if (file.good()){
+        string line;
+        stringstream result("");
+        while(getline(file, line)) {
+            result << line;
+            result << '\n';
+        }
+        fileContents = result.str();
+    } else {
+        cout << fileName << " doesn't exist!" << endl;
+        return;
+    }
+    file.close();
 }
