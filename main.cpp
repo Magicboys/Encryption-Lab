3edfc24c5b86723aca48f978e013cad5f6210775
Magicboyd <captaintylerwatson@hotmail.com>
2023-02-08 21:16:58 -0800

    Shift Row Step Complete

--- main.cpp
+++ main.cpp
@@ -24,7 +24,24 @@ void WriteToFile(string fileContents);
 void EncryptString(string originalString, string& encryptedString);
 void decryptString(string encryptedString, string& decryptedString);
 
-//Helper Methods
+//Global Constants
+//S-BOX
+const string SBOX [16][16] = {{"63", "7c", "77", "7b","f2", "6b", "6f", "c5", "30", "01", "67", "2b", "fe", "d7", "ab", "76"},
+                        {"ca", "82", "c9", "7d","fa", "59", "47", "f0", "ad", "d4", "a2", "af", "9c", "a4", "72", "c0"},
+                        {"b7", "fd", "93", "26","36", "3f", "f7", "cc", "34", "a5", "e5", "f1", "71", "d8", "31", "15"},
+                        {"04", "c7", "23", "c3","18", "96", "05", "9a", "07", "12", "80", "e2", "eb", "27", "b2", "75"},
+                        {"09", "83", "2c", "1a","1b", "6e", "5a", "a0", "52", "3b", "d6", "b3", "29", "e3", "2f", "84"},
+                        {"53", "d1", "00", "ed","20", "fc", "b1", "5b", "6a", "cb", "be", "39", "4a", "4c", "58", "cf"},
+                        {"d0", "ef", "aa", "fb","43", "4d", "33", "85", "45", "f9", "02", "7f", "50", "3c", "9f", "a8"},
+                        {"51", "a3", "40", "8f","92", "9d", "38", "f5", "bc", "b6", "da", "21", "10", "ff", "f3", "d2"},
+                        {"cd", "0c", "13", "ec","5f", "97", "44", "17", "c4", "a7", "7e", "3d", "64", "5d", "19", "73"},
+                        {"60", "81", "4f", "dc","22", "2a", "90", "88", "46", "ee", "b8", "14", "de", "5e", "0b", "db"},
+                        {"e0", "37", "3a", "0a","49", "06", "24", "5c", "c2", "d3", "ac", "62", "91", "95", "e4", "79"},
+                        {"e7", "c8", "37", "6d","8d", "d5", "4e", "a9", "6c", "56", "f4", "ea", "65", "7a", "ae", "08"},
+                        {"ba", "78", "25", "2e","1c", "a6", "b4", "c6", "e8", "dd", "74", "1f", "4b", "bd", "8b", "8a"},
+                        {"70", "3e", "b5", "66","48", "03", "f6", "0e", "61", "35", "57", "b9", "86", "c1", "1d", "9e"},
+                        {"e1", "f8", "98", "11","69", "d9", "8e", "94", "9b", "1e", "87", "e9", "cd", "55", "28", "df"},
+                        {"8c", "a1", "89", "0d","bf", "e6", "42", "68", "41", "99", "2d", "0f", "b0", "54", "bb", "16"}};
 
 
 int main(int argc, char* argv[]) {
@@ -65,6 +82,7 @@ int main(int argc, char* argv[]) {
 
 
 void EncryptString(string originalString, string& encryptedString) {
+    //TODO: make the matrix based off the original string
     //First make original state matrix
     string state[4][4];
     int indexCounter = 0;
@@ -79,8 +97,8 @@ void EncryptString(string originalString, string& encryptedString) {
         }
     }
 
-    //ADD ROW KEY LOGIC
-    //Later we should make the key matrix, but this is simple for now
+    //STEP 0: AddRoundKey
+    //TODO: Later we should make the key matrix, but this is simple for now
     string key[4] = {"c", "62", "61", "50"};
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
@@ -94,7 +112,21 @@ void EncryptString(string originalString, string& encryptedString) {
         }
     }
 
-    //Debug
+    //TODO: Encapsulate within a loop which runs steps 1-4 ten rounds
+//    for (int round = 0; round < 10; round++) {
+//
+//    }
+
+    //STEP 1: SUB BYTE STEP
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            int x = stoi(state[i][j].substr(0,1), nullptr, 16);
+            int y = stoi(state[i][j].substr(1,1), nullptr, 16);
+            state[i][j] = SBOX[x][y];
+        }
+    }
+
+    //TODO: DEBUG (Remove Later)
     for (int i = 0; i < 4; i++) {
         cout << "[";
         for (int j = 0; j < 4; j++) {
@@ -103,39 +135,49 @@ void EncryptString(string originalString, string& encryptedString) {
         cout << "]" << endl;
     }
 
-    //S-BOX
-    string SBOX [16][16] = {{"63", "7c", "77", "7b","f2", "6b", "6f", "c5", "30", "01", "67", "2b", "fe", "d7", "ab", "76"},
-                            {"ca", "82", "c9", "7d","fa", "59", "47", "f0", "ad", "d4", "a2", "af", "9c", "a4", "72", "c0"},
-                            {"b7", "fd", "93", "26","36", "3f", "f7", "cc", "34", "a5", "e5", "f1", "71", "d8", "31", "15"},
-                            {"04", "c7", "23", "c3","18", "96", "05", "9a", "07", "12", "80", "e2", "eb", "27", "b2", "75"},
-                            {"09", "83", "2c", "1a","1b", "6e", "5a", "a0", "52", "3b", "d6", "b3", "29", "e3", "2f", "84"},
-                            {"53", "d1", "00", "ed","20", "fc", "b1", "5b", "6a", "cb", "be", "39", "4a", "4c", "58", "cf"},
-                            {"d0", "ef", "aa", "fb","43", "4d", "33", "85", "45", "f9", "02", "7f", "50", "3c", "9f", "a8"},
-                            {"51", "a3", "40", "8f","92", "9d", "38", "f5", "bc", "b6", "da", "21", "10", "ff", "f3", "d2"},
-                            {"cd", "0c", "13", "ec","5f", "97", "44", "17", "c4", "a7", "7e", "3d", "64", "5d", "19", "73"},
-                            {"60", "81", "4f", "dc","22", "2a", "90", "88", "46", "ee", "b8", "14", "de", "5e", "0b", "db"},
-                            {"e0", "37", "3a", "0a","49", "06", "24", "5c", "c2", "d3", "ac", "62", "91", "95", "e4", "79"},
-                            {"e7", "c8", "37", "6d","8d", "d5", "4e", "a9", "6c", "56", "f4", "ea", "65", "7a", "ae", "08"},
-                            {"ba", "78", "25", "2e","1c", "a6", "b4", "c6", "e8", "dd", "74", "1f", "4b", "bd", "8b", "8a"},
-                            {"70", "3e", "b5", "66","48", "03", "f6", "0e", "61", "35", "57", "b9", "86", "c1", "1d", "9e"},
-                            {"e1", "f8", "98", "11","69", "d9", "8e", "94", "9b", "1e", "87", "e9", "cd", "55", "28", "df"},
-                            {"8c", "a1", "89", "0d","bf", "e6", "42", "68", "41", "99", "2d", "0f", "b0", "54", "bb", "16"}};
-    //SUB BYTE STEP
+    //STEP 2: SHIFT ROW STEP
     for (int i = 0; i < 4; i++) {
+        //Grab original row
+        string originalRow[4];
         for (int j = 0; j < 4; j++) {
-            int x = stoi(state[i][j].substr(0,1), nullptr, 16);
-            int y = stoi(state[i][j].substr(1,1), nullptr, 16);
-            state[i][j] = SBOX[x][y];
+            originalRow[j] = state[i][j];
+        }
+
+        //Shift the bytes
+        string shiftedRow[4];
+        int shiftedIndex = (i);
+        for (int j = 0; j < 4; j++) {
+            shiftedRow[j] = originalRow[shiftedIndex];
+            shiftedIndex++;
+            if (shiftedIndex >= 4) {
+                shiftedIndex = 0;
+            }
+        }
+
+        //Save the shifted row
+        for (int j = 0; j < 4; j++) {
+            state[i][j] = shiftedRow[j];
         }
     }
-    //TODO: 2. SHIFT ROW STEP
-    
 
     //TODO: 3. MIX COLUMNS STEP
 
-    //TODO: 4. ADD ROUND KEY
+    //STEP 4: AddRoundKey
+    //TODO: Later we should make the key matrix, but this is simple for now
+    string stepFourKey[4] = {"d", "49", "32", "b4"};
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            int a = stoi(stepFourKey[j], nullptr, 16);
+            int b = stoi(state[i][j], nullptr, 16);
+            ostringstream oss;
+            string hex_representation;
+            oss << std::hex << int((a+b));
+            string hex_sum = oss.str();
+            state[i][j] = hex_sum;
+        }
+    }
 
-    //Debug
+    //TODO: DEBUG (Remove Later)
     cout << "====================" << endl;
     for (int i = 0; i < 4; i++) {
         cout << "[";
@@ -145,6 +187,7 @@ void EncryptString(string originalString, string& encryptedString) {
         cout << "]" << endl;
     }
 
+    //TODO: Add round ten after round loop and then return the string via pass by reference encryptedString param. Round 10 should include steps 1,2 & 4.
 }
 
 //Purpose: takes in the name of a file and reads its contents (if the file exists)
